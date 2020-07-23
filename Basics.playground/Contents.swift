94f526afebe51aad380bd62cf20d8e4717c1a357
mildchae <chaeefe@gmail.com>
2020-07-23 22:17:03 +0900

    basic

--- Basics.playground/Contents.swift
+++ Basics.playground/Contents.swift
@@ -2,4 +2,16 @@ import UIKit
 // Basics
 
 
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+var `break` = 10
 
