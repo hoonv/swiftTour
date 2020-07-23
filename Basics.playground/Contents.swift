f72c33c0f9cc7586d690dbc4845eccec7ef426e2
mildchae <chaeefe@gmail.com>
2020-07-23 23:49:51 +0900

    basic

--- Basics.playground/Contents.swift
+++ Basics.playground/Contents.swift
@@ -15,3 +15,11 @@ var red, green, blue: Double
 
 var `break` = 10
 
+let int16max = Int16.max
+let intmax = Int32.max
+let int32min = Int32.min
+let uintmax = UInt32.max
+
+var int8 = 10
+var int16 = 10000
+print(int8+int16)
